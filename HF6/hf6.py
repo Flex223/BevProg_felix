26417eaea8829702dcd1b54a0e252d6b5772bcc5
Flex223 <63854067+Flex223@users.noreply.github.com>
2022-10-29 14:27:27 +0200

    Delete HF6 directory

--- HF6/hf6.py
+++
@@ -1,9 +0,0 @@
-print("\t", end="")
-for i in range(1,13):
-    print('%5i' % i, end="")
-print("\n :--------------------------------------------------------------")
-for i in range(1,13):
-    print(str(i) + ":\t", end="")
-    for y in range(1,13,2):
-        print('%5i' % (i*y), '%4i' % (i*(y+1)), end="")
-    print("\n")
